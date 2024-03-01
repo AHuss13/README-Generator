527e5b172fbdc505ae518ea85c2532ea64a87aa9
Adam Huss <adamhuss13@gmail.com>
2024-03-01 15:37:04 -0600

    fixed package requirement and license option, removed readme comments, and added functions to write and call application

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Include packages needed for this application
 
-const fs = require('fs)');
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
 
@@ -27,10 +27,10 @@ const questions = [
     message: 'How do you use your project?',
   },
   {
-    type: 'input',
+    type: 'list',
     name: 'license',
     message: 'What License does your project use?',
-    choices: [MIT, GNU, None],
+    choices: ['MIT', 'GNU_GPLv3', 'None'],
   },
   {
     type: 'input',
@@ -45,7 +45,7 @@ const questions = [
   {
     type: 'input',
     name: 'github',
-    message: 'I beseech thee to provide thine Github name of usage here:',
+    message: 'I beseech thee to provide thy Github name of usage here:',
   },
   {
     type: 'input',
@@ -54,50 +54,20 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md generated!')
+  );
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const write = generateMarkdown(answers);
+    writeToFile('README.md', write);
+  });
+};
 
 // Function call to initialize app
 init();
-
-
-
-
-
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections
-//      entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the
-//      README entitled License that explains which license the application is covered under
-
-
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
